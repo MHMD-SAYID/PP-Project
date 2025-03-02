728f4f08ed34fd74dbfe4243c1715ccdf1762b59
Mohamed Sayid <m.alkordy2003@gmail.com>
2025-03-02 22:50:50 +0200

    add download folder

--- src/DwonloadTask.java
+++ src/DwonloadTask.java
@@ -1,10 +1,10 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
+import java.nio.file.Paths;
 
 class DownloadTask implements Runnable {
     private final String fileUrl;
+    private static final String DOWNLOAD_FOLDER = "downloads/";
 
     public DownloadTask(String fileUrl) {
         this.fileUrl = fileUrl;
@@ -21,17 +21,29 @@ class DownloadTask implements Runnable {
 
     private void downloadFile(String fileUrl) throws IOException {
         URL url = new URL(fileUrl);
-        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
 
+        // Extract file name safely
+        String fileName = Paths.get(url.getPath()).getFileName().toString();
+
+        // Remove invalid characters
+        fileName = fileName.replaceAll("[^a-zA-Z0-9.\\-_]", "_"); // Replaces invalid chars with "_"
+
+        // Ensure the download directory exists
+        File dir = new File(DOWNLOAD_FOLDER);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        File file = new File(DOWNLOAD_FOLDER + fileName);
         try (InputStream in = url.openStream();
-             FileOutputStream fos = new FileOutputStream(fileName)) {
+             FileOutputStream fos = new FileOutputStream(file)) {
 
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = in.read(buffer)) != -1) {
                 fos.write(buffer, 0, bytesRead);
             }
-            System.out.println("Downloaded: " + fileName);
+            System.out.println("Downloaded: " + file.getAbsolutePath());
         }
     }
 }
